c6882bf9d8fa3381b21adab28a679f2cf975cb51
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2024-12-29 00:37:15 -0300

    home previews

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">
-          Smith
+          Pizarro
         </a>
 
         <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
