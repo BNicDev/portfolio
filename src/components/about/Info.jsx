a68796f0bf0cf4450ae69741e9df2de96d502bf7
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2025-01-02 21:44:48 -0300

    about section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+        <div className="about__box">
+        <img className='about__icon' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAALJJREFUSEvtldENgzAQQx+bwCZlMtRJWKWbtJsAQUVKqqQ+Ton4IT/5cM6OfXDpaLy6xvwogQcwAWHPrRfw3PCwZ5cSeAO9cBnIR6/A8i0sXUThMiJFoPBEQOVt/R6SvsTWLXmfEdn7Egscdq0k6tzOfbnAcYGSu3+4yUFVAZWpC1c9qOogl3NzAVcs8XhREd0CyZ9ccxZ9gOF3VIRpOhseGNWX4jRVhS5cPZku0rioucAKApQlGWm0QmwAAAAASUVORK5CYII="/>
+            <h3 className="about__title">Experience</h3>
+            <span className="about__subtitle">2 Years Working</span>
+        </div>
+        <div className="about__box">
+        <img className='about__icon' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAR5JREFUSEvNlIENwlAIROkmuolOok6iTqJOok6im2hfAklN4UPTNJHkR22F4+74dLJwdAvXlyrAUURWIrLRht4icut/P0SE72FkABS8aHGvCMUPCuSCtAAofney1goIK2PEM5dJC+AVdD7MAeSkxbceSARgiZEsV/WArq0RpOL5T0QASGP0IwPp/Nx3vVefKA5ICeBTHF8aZLpgARu8KAFE+rdwJwEwmlCfEtwJjC4xMF2nAJgnJQB0xWg+K+HKQ2LrHkwBQRokGsXcVUHBsHjGYNgNF89MhxndcrgHzcgYZPnp+wzAFhqrGSPtcMthssuYVAAYv1a442kJfwfAQkMaOzQ6mwHFns4qxgc8wp/Rmq5KlE5J9ofMgyw/ff8F6287Gbw3f80AAAAASUVORK5CYII="/>
+            <h3 className="about__title">Certificates</h3>
+            <span className="about__subtitle">4 + Cerificates</span>
+        </div>
+        <div className="about__box">
+        <img className='about__icon' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAPNJREFUSEvtlYENgjAQRT+b6CY6iTqJOoluIqO4ifJMj1wq5SpKTIyXEAJt/7v7V0qjmaOZWV+/CVhJOklaTLTvKmknqWX9kEWXbhDIOwFkWQLckjKTzl0Sx0rSNlVu0x/JD1VgAJt4qIBgKQAfIQBhLoJq1unuRegT4mYp3vPcJz9WAWMI0BPuuWXeEt9YcyCswOCIb1w1VMU7s4Q+kblFCCj1dO8gNgdhAD6KgLy5Tw1Lmds30u/1TwJqduxki2rEmRMCSlbVHozfB0RWjG0GW8tBx4cZ/g9yMWyKAOFpOrSn7V1tH3qNlxdE/uXjf0Do2B3QQTQZHcNWzQAAAABJRU5ErkJggg=="/>
+            <h3 className="about__title">Support</h3>
+            <span className="about__subtitle">Online 24/7</span>
+        </div>
+    </div>
+  )
+}
+
+export default Info
