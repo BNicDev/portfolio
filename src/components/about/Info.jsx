d5716861d697746b567fc024cd7ebe2f7ac540ee
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2025-01-04 11:10:00 -0300

    skills

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,7 +6,7 @@ const Info = () => {
         <div className="about__box">
         <img className='about__icon' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAALJJREFUSEvtldENgzAQQx+bwCZlMtRJWKWbtJsAQUVKqqQ+Ton4IT/5cM6OfXDpaLy6xvwogQcwAWHPrRfw3PCwZ5cSeAO9cBnIR6/A8i0sXUThMiJFoPBEQOVt/R6SvsTWLXmfEdn7Egscdq0k6tzOfbnAcYGSu3+4yUFVAZWpC1c9qOogl3NzAVcs8XhREd0CyZ9ccxZ9gOF3VIRpOhseGNWX4jRVhS5cPZku0rioucAKApQlGWm0QmwAAAAASUVORK5CYII="/>
             <h3 className="about__title">Experience</h3>
-            <span className="about__subtitle">2 Years Working</span>
+            <span className="about__subtitle">1 Year Working</span>
         </div>
         <div className="about__box">
         <img className='about__icon' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAR5JREFUSEvNlIENwlAIROkmuolOok6iTqJOok6im2hfAklN4UPTNJHkR22F4+74dLJwdAvXlyrAUURWIrLRht4icut/P0SE72FkABS8aHGvCMUPCuSCtAAofney1goIK2PEM5dJC+AVdD7MAeSkxbceSARgiZEsV/WArq0RpOL5T0QASGP0IwPp/Nx3vVefKA5ICeBTHF8aZLpgARu8KAFE+rdwJwEwmlCfEtwJjC4xMF2nAJgnJQB0xWg+K+HKQ2LrHkwBQRokGsXcVUHBsHjGYNgNF89MhxndcrgHzcgYZPnp+wzAFhqrGSPtcMthssuYVAAYv1a442kJfwfAQkMaOzQ6mwHFns4qxgc8wp/Rmq5KlE5J9ofMgyw/ff8F6287Gbw3f80AAAAASUVORK5CYII="/>
